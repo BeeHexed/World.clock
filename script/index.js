df9975e14f9a5f3b099b0af5ef29d4cea55b2da4
BeeHexed <livingfree303@gmail.com>
2025-01-24 21:06:21 +0000

    fixed time display

--- script/index.js
+++ script/index.js
@@ -2,6 +2,6 @@ let today = moment().format("dddd, MMMM D, YYYY");
 let todayElement = document.querySelector("#date");
 todayElement.innerHTML = `${today}`;
 
-let currentTime = moment().format("h:mm:s");
+let currentTime = moment().format("hh:mm:ss");
 let timeElement = document.querySelector("#time");
 timeElement.innerHTML = `${currentTime}`;
