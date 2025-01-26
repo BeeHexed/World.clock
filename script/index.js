9ec5a05d7adc1fb02a5625c3633cdbf2b03bec9d
BeeHexed <livingfree303@gmail.com>
2025-01-26 22:31:30 +0000

    Updated functionality

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
-let today = moment().format("dddd, MMMM D, YYYY");
-let todayElement = document.querySelector("#date");
-todayElement.innerHTML = `${today}`;
+let firstCity = moment().format("dddd, MMMM D, YYYY");
+let cityElement = document.querySelector("#firstCity");
+cityElement.innerHTML = `${firstCity}`;
 
 let currentTime = moment().format("hh:mm:ss");
 let timeElement = document.querySelector("#time");
