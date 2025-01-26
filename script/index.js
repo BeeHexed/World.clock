74eb76fdede0fb2cbf1b2d13138909bdd91acbed
BeeHexed <livingfree303@gmail.com>
2025-01-26 00:26:32 +0000

    added 2nd city moment

--- script/index.js
+++ script/index.js
@@ -5,3 +5,7 @@ todayElement.innerHTML = `${today}`;
 let currentTime = moment().format("hh:mm:ss");
 let timeElement = document.querySelector("#time");
 timeElement.innerHTML = `${currentTime}`;
+
+let tomorrow = moment().add(1, "days").format("dddd, MMMM D, YYYY");
+let tomorrowElement = document.querySelector("#tomorrow");
+tomorrowElement.innerHTML = `${tomorrow}`;
