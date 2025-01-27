e23b8c9f2b8950687d344b755be87b292d0dae65
BeeHexed <livingfree303@gmail.com>
2025-01-27 21:23:19 +0000

    Pause point

--- script/index.js
+++ script/index.js
@@ -1,3 +1,26 @@
+/*
+stop here. Reapproach in am
+function selectedCity(event) {
+  const selectedCity = event.target.value;
+  {
+    if (selectedCity === "london") {
+      alert("It is Saturday, October 8, 2023 2:51 PM in Europe/Paris");
+    } else if (selectedCity === "paris") {
+      alert("💙");
+    } else if (selectedCity === "tokyo") {
+      alert("💛");
+    }
+  }
+}
+
+const citySelect = document.querySelector("#city");
+
+citySelect.addEventListener("change", selectedCity);
+*/
+
+
+
+
 let firstCity = moment().format("dddd, MMMM D, YYYY");
 let cityElement = document.querySelector("#firstCity");
 cityElement.innerHTML = `${firstCity}`;
@@ -9,3 +32,6 @@ timeElement.innerHTML = `${currentTime}`;
 let tomorrow = moment().add(1, "days").format("dddd, MMMM D, YYYY");
 let tomorrowElement = document.querySelector("#tomorrow");
 tomorrowElement.innerHTML = `${tomorrow}`;
+
+
+
