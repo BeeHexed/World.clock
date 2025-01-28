fd37d308ad615011eba85e1dfc1fb14dc0835484
BeeHexed <livingfree303@gmail.com>
2025-01-28 22:09:26 +0000

    restored to functional point

--- script/index.js
+++ script/index.js
@@ -39,9 +39,5 @@ let currentTime = moment().format("hh:mm:ss");
 let timeElement = document.querySelector("#time");
 timeElement.innerHTML = `${currentTime}`;
 
-let tomorrow = moment().add(1, "days").format("dddd, MMMM D, YYYY");
-let tomorrowElement = document.querySelector("#tomorrow");
-tomorrowElement.innerHTML = `${tomorrow}`;
-
 
 
