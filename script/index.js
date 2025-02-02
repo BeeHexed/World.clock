9df1e6840829c6237d71e3c262264864c409ff72
BeeHexed <livingfree303@gmail.com>
2025-02-02 21:59:55 +0000

    Swapped NYC and Rome for better time display

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 function updateTime(){
-//London{
+//London
 let firstCityDateElement = document.querySelector("#first-date");
 let firstCityTimeElement = document.querySelector("#first-time");
 let timeOne = moment().tz("Europe/London");
@@ -7,10 +7,10 @@ firstCityDateElement.innerHTML= timeOne.format("ddd., MMMM Do YYYY");
 firstCityTimeElement.innerHTML = timeOne.format(`h:mm:ss [<small>]A[</small>]`);
 
 
-//New York
+//Rome
 let secondCityDateElement = document.querySelector("#second-date");
 let secondCityTimeElement = document.querySelector("#second-time");
-let timeTwo = moment().tz("America/New_York");
+let timeTwo = moment().tz("Europe/Rome");
 secondCityDateElement.innerHTML= timeTwo.format("ddd., MMMM Do YYYY");
 secondCityTimeElement.innerHTML = timeTwo.format(`h:mm:ss [<small>]A[</small>]`);
 
@@ -21,13 +21,12 @@ let timeThree = moment().tz("Asia/Tokyo");
 thirdCityDateElement.innerHTML= timeThree.format("ddd., MMMM Do YYYY");
 thirdCityTimeElement.innerHTML = timeThree.format(`h:mm:ss [<small>]A[</small>]`);
 
-//Rome
+//New York
 let fourthCityDateElement = document.querySelector("#fourth-date");
 let fourthCityTimeElement = document.querySelector("#fourth-time");
-let timeFour = moment().tz("Europe/Rome");
+let timeFour = moment().tz("America/New_York");
 fourthCityDateElement.innerHTML= timeFour.format("ddd., MMMM Do YYYY");
 fourthCityTimeElement.innerHTML = timeFour.format(`h:mm:ss [<small>]A[</small>]`);
-
 }
 
 updateTime();
