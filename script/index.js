4175ba0a977a4f99ced0f44fb8f921e61266cf83
BeeHexed <livingfree303@gmail.com>
2025-02-02 21:12:39 +0000

    set timeInterval

--- script/index.js
+++ script/index.js
@@ -1,20 +1,24 @@
 
 //London
+setInterval(function() {
 let firstCityDateElement = document.querySelector("#first-date");
 let firstCityTimeElement = document.querySelector("#first-time");
 let timeOne = moment().tz("Europe/London");
 firstCityDateElement.innerHTML= timeOne.format("dddd, MMMM Do YYYY");
-firstCityTimeElement.innerHTML = timeOne.format(`h:mm:ss [<small>]A[</small>]`);
+firstCityTimeElement.innerHTML = timeOne.format(`h:mm:ss [<small>]A[</small>]`)
+}, 1000);
 
 
 
 
 //New York
+setInterval(function(){
 let secondCityDateElement = document.querySelector("#second-date");
 let secondCityTimeElement = document.querySelector("#second-time");
 let timeTwo = moment().tz("America/New_York");
 secondCityDateElement.innerHTML= timeTwo.format("dddd, MMMM Do YYYY");
-secondCityTimeElement.innerHTML = timeTwo.format(`h:mm:ss [<small>]A[</small>]`);
+secondCityTimeElement.innerHTML = timeTwo.format(`h:mm:ss [<small>]A[</small>]`)
+}, 1000);
 
 
 
