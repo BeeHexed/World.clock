a9273afd76a44322892b46b1034ec7cde267414d
BeeHexed <livingfree303@gmail.com>
2025-02-02 21:53:49 +0000

    notes and spacing

--- script/index.js
+++ script/index.js
@@ -3,32 +3,30 @@ function updateTime(){
 let firstCityDateElement = document.querySelector("#first-date");
 let firstCityTimeElement = document.querySelector("#first-time");
 let timeOne = moment().tz("Europe/London");
-firstCityDateElement.innerHTML= timeOne.format("dddd, MMMM Do YYYY");
+firstCityDateElement.innerHTML= timeOne.format("ddd., MMMM Do YYYY");
 firstCityTimeElement.innerHTML = timeOne.format(`h:mm:ss [<small>]A[</small>]`);
 
 
 //New York
-setInterval(function(){
 let secondCityDateElement = document.querySelector("#second-date");
 let secondCityTimeElement = document.querySelector("#second-time");
 let timeTwo = moment().tz("America/New_York");
-secondCityDateElement.innerHTML= timeTwo.format("dddd, MMMM Do YYYY");
-secondCityTimeElement.innerHTML = timeTwo.format(`h:mm:ss [<small>]A[</small>]`)
-}, 1000);
+secondCityDateElement.innerHTML= timeTwo.format("ddd., MMMM Do YYYY");
+secondCityTimeElement.innerHTML = timeTwo.format(`h:mm:ss [<small>]A[</small>]`);
 
 //Tokyo
 let thirdCityDateElement = document.querySelector("#third-date");
 let thirdCityTimeElement = document.querySelector("#third-time");
 let timeThree = moment().tz("Asia/Tokyo");
-thirdCityDateElement.innerHTML= timeThree.format("dddd, MMMM Do YYYY");
-thirdCityTimeElement.innerHTML = timeThree.format(`h:mm:ss [<small>]A[</small>]`)
+thirdCityDateElement.innerHTML= timeThree.format("ddd., MMMM Do YYYY");
+thirdCityTimeElement.innerHTML = timeThree.format(`h:mm:ss [<small>]A[</small>]`);
 
 //Rome
 let fourthCityDateElement = document.querySelector("#fourth-date");
 let fourthCityTimeElement = document.querySelector("#fourth-time");
 let timeFour = moment().tz("Europe/Rome");
-fourthCityDateElement.innerHTML= timeFour.format("dddd, MMMM Do YYYY");
-fourthCityTimeElement.innerHTML = timeFour.format(`h:mm:ss [<small>]A[</small>]`)
+fourthCityDateElement.innerHTML= timeFour.format("ddd., MMMM Do YYYY");
+fourthCityTimeElement.innerHTML = timeFour.format(`h:mm:ss [<small>]A[</small>]`);
 
 }
 
