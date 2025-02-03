c54b5fae3fcb3758d552f523df6e0257f2f5476b
BeeHexed <livingfree303@gmail.com>
2025-02-03 16:58:25 +0000

    individual setIntervals for each city

--- script/index.js
+++ script/index.js
@@ -1,34 +1,37 @@
-function updateTime(){
+setInterval(function() {
 //London
 let firstCityDateElement = document.querySelector("#first-date");
 let firstCityTimeElement = document.querySelector("#first-time");
 let timeOne = moment().tz("Europe/London");
 firstCityDateElement.innerHTML= timeOne.format("ddd., MMMM Do YYYY");
 firstCityTimeElement.innerHTML = timeOne.format(`h:mm:ss [<small>]A[</small>]`);
+}, 1000);
 
 
+setInterval(function() {
 //Rome
 let secondCityDateElement = document.querySelector("#second-date");
 let secondCityTimeElement = document.querySelector("#second-time");
 let timeTwo = moment().tz("Europe/Rome");
 secondCityDateElement.innerHTML= timeTwo.format("ddd., MMMM Do YYYY");
 secondCityTimeElement.innerHTML = timeTwo.format(`h:mm:ss [<small>]A[</small>]`);
+}, 1000);
 
+setInterval(function() {
 //New York
 let thirdCityDateElement = document.querySelector("#third-date");
 let thirdCityTimeElement = document.querySelector("#third-time");
 let timeThree = moment().tz("America/New_York");
 thirdCityDateElement.innerHTML= timeThree.format("ddd., MMMM Do YYYY");
 thirdCityTimeElement.innerHTML = timeThree.format(`h:mm:ss [<small>]A[</small>]`);
+}, 1000);
 
+setInterval(function() {
 //Tokyo
 let fourthCityDateElement = document.querySelector("#fourth-date");
 let fourthCityTimeElement = document.querySelector("#fourth-time");
 let timeFour = moment().tz("Asia/Tokyo");
 fourthCityDateElement.innerHTML= timeFour.format("ddd., MMMM Do YYYY");
 fourthCityTimeElement.innerHTML = timeFour.format(`h:mm:ss [<small>]A[</small>]`);
-}
-
-updateTime();
-setInterval(updateTime, 1000);
+}, 1000);
 
