df5b184231a6c17d3cef7a54922800b30b62be61
BeeHexed <livingfree303@gmail.com>
2025-02-08 14:32:31 +0000

    removed day of week from JS

--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@ setInterval(function() {
 let firstCityDateElement = document.querySelector("#first-date");
 let firstCityTimeElement = document.querySelector("#first-time");
 let timeOne = moment().tz("Europe/London");
-firstCityDateElement.innerHTML= timeOne.format("ddd., MMMM Do YYYY");
+firstCityDateElement.innerHTML= timeOne.format("MMMM Do, YYYY");
 firstCityTimeElement.innerHTML = timeOne.format(`h:mm:ss [<small>]A[</small>]`);
 }, 1000);
 
@@ -13,7 +13,7 @@ setInterval(function() {
 let secondCityDateElement = document.querySelector("#second-date");
 let secondCityTimeElement = document.querySelector("#second-time");
 let timeTwo = moment().tz("Europe/Rome");
-secondCityDateElement.innerHTML= timeTwo.format("ddd., MMMM Do YYYY");
+secondCityDateElement.innerHTML= timeTwo.format("MMMM Do, YYYY");
 secondCityTimeElement.innerHTML = timeTwo.format(`h:mm:ss [<small>]A[</small>]`);
 }, 1000);
 
@@ -22,7 +22,7 @@ setInterval(function() {
 let thirdCityDateElement = document.querySelector("#third-date");
 let thirdCityTimeElement = document.querySelector("#third-time");
 let timeThree = moment().tz("America/New_York");
-thirdCityDateElement.innerHTML= timeThree.format("ddd., MMMM Do YYYY");
+thirdCityDateElement.innerHTML= timeThree.format("MMMM Do, YYYY");
 thirdCityTimeElement.innerHTML = timeThree.format(`h:mm:ss [<small>]A[</small>]`);
 }, 1000);
 
@@ -31,7 +31,7 @@ setInterval(function() {
 let fourthCityDateElement = document.querySelector("#fourth-date");
 let fourthCityTimeElement = document.querySelector("#fourth-time");
 let timeFour = moment().tz("Asia/Tokyo");
-fourthCityDateElement.innerHTML= timeFour.format("ddd., MMMM Do YYYY");
+fourthCityDateElement.innerHTML= timeFour.format("MMMM Do, YYYY");
 fourthCityTimeElement.innerHTML = timeFour.format(`h:mm:ss [<small>]A[</small>]`);
 }, 1000);
 
