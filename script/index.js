98798d5f28d9c4685dc0cdf811fa8613314fbb80
BeeHexed <livingfree303@gmail.com>
2025-02-13 20:36:56 +0000

    comma

--- script/index.js
+++ script/index.js
@@ -5,7 +5,7 @@ setInterval(function() {
 let firstCityDateElement = document.querySelector("#first-date");
 let firstCityTimeElement = document.querySelector("#first-time");
 let timeOne = moment().tz("Europe/London");
-firstCityDateElement.innerHTML= timeOne.format("Do MMMM, YYYY");
+firstCityDateElement.innerHTML= timeOne.format("Do MMMM YYYY");
 firstCityTimeElement.innerHTML = timeOne.format(`h:mm:ss [<small>]A[</small>]`);} , 1000);
 
 //Rome
@@ -14,7 +14,7 @@ setInterval(function() {
 let secondCityDateElement = document.querySelector("#second-date");
 let secondCityTimeElement = document.querySelector("#second-time");
 let timeTwo = moment().tz("Europe/Rome");
-secondCityDateElement.innerHTML= timeTwo.format("Do MMMM, YYYY");
+secondCityDateElement.innerHTML= timeTwo.format("Do MMMM YYYY");
 secondCityTimeElement.innerHTML = timeTwo.format(`h:mm:ss [<small>]A[</small>]`); } , 1000);
 
 
@@ -24,7 +24,7 @@ setInterval(function() {
 let thirdCityDateElement = document.querySelector("#third-date");
 let thirdCityTimeElement = document.querySelector("#third-time");
 let timeThree = moment().tz("America/New_York");
-thirdCityDateElement.innerHTML= timeThree.format("Do MMMM, YYYY");
+thirdCityDateElement.innerHTML= timeThree.format("Do MMMM YYYY");
 thirdCityTimeElement.innerHTML = timeThree.format(`h:mm:ss [<small>]A[</small>]`); } , 1000);
 
 //Tokyo
@@ -33,7 +33,7 @@ setInterval(function() {
 let fourthCityDateElement = document.querySelector("#fourth-date");
 let fourthCityTimeElement = document.querySelector("#fourth-time");
 let timeFour = moment().tz("Asia/Tokyo");
-fourthCityDateElement.innerHTML= timeFour.format("Do MMMM, YYYY");
+fourthCityDateElement.innerHTML= timeFour.format("Do MMMM YYYY");
 fourthCityTimeElement.innerHTML = timeFour.format(`h:mm:ss [<small>]A[</small>]`);
 }, 1000);
 
@@ -44,7 +44,7 @@ function updateCity(event) {
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities")
   citiesElement.innerHTML = `
-     <div class="date" id="first-date">${moment().tz(cityTimeZone).format("Do MMMM, YYYY ")}</div>
+     <div class="date" id="first-date">${moment().tz(cityTimeZone).format("Do MMMM YYYY ")}</div>
         <div class="timeDisplay" id="first-time">${moment().tz(cityTimeZone).format("HH:mm:ss")}</div>`
 };
 
